a2fa61136c4f3525a3a181cbe0341f4e0a10f090
ramchandra-ghub <74346094+ramchandra-ghub@users.noreply.github.com>
2024-08-12 18:49:53 -0400

    Update script2.js

--- scripts/Landlord/script2.js
+++ scripts/Landlord/script2.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 export const options = {
   vus: 1,
-  iterations:1
+  iterations:1,
 //   duration: '30s',
   thresholds:{
     'group_duration{group:::Admin_launch}': ['max>=0'],
