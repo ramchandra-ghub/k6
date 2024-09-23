61be8168c4bc931b78575a1515c4afd13a0cd482
ramchandra-ghub <74346094+ramchandra-ghub@users.noreply.github.com>
2024-09-23 16:29:48 -0400

    Update script2.js

--- scripts/Landlord/script2.js
+++ scripts/Landlord/script2.js
@@ -17,17 +17,17 @@ export default function () {
   sleep(1);
 }
 
-export function handleSummary(data) {
-  let csvContent = `Group, 90 Percentile(ms) \n`
-  for(let metricName in data.metrics){
-    if(metricName.startsWith('group_duration{group:::')){
-      let groupName = metricName.match(/group:::(.+?)}/)
-      let percentile90 = data.metrics[metricName].values['p(90)']
-      csvContent += `${groupName[1]},${percentile90}\n`
-    }
-  }
-  return {
-    'csvContent.csv': csvContent,
-  }
+// export function handleSummary(data) {
+//   let csvContent = `Group, 90 Percentile(ms) \n`
+//   for(let metricName in data.metrics){
+//     if(metricName.startsWith('group_duration{group:::')){
+//       let groupName = metricName.match(/group:::(.+?)}/)
+//       let percentile90 = data.metrics[metricName].values['p(90)']
+//       csvContent += `${groupName[1]},${percentile90}\n`
+//     }
+//   }
+//   return {
+//     'csvContent.csv': csvContent,
+//   }
 
-}
+// }
